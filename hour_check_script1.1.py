4f50911f0bbb5b9f744fbf56fd868b5d6e369840
mludwig <mjbludwig@gmail.com>
2018-04-12 16:52:24 -0400

    Changed main function to open the argument file at the top and close it after running it through all of the functions as a parameter. Before every function would open and close the file.

--- hour_check_script1.1.py
+++ hour_check_script1.1.py
@@ -4,69 +4,74 @@ import sys
 
 
 def main():
-
-    checkHourIncrement(fileName)
-    checkFileDate(fileName)
-    checkForBlanks(fileName)
-    nameMatchCheck(fileName)
+    csvFile = open(fileName, 'r')
+    checkForOverlap(csvFile)
+    checkHourIncrement(csvFile)
+    checkFileDate(csvFile)
+    checkForBlanks(csvFile)
+    nameMatchCheck(csvFile)
     global errors
-    #print(str(errors))
     if errors == 1:
         sys.exit(1)
     elif errors == 0:
         sys.exit(0)
+    csvFile.close()
 
 
 def checkHourIncrement(fileName):
-    with open(fileName, 'r') as csvFile:
-        reader = csv.reader(csvFile, delimiter='|')
-        rowNum = 1
-        for row in reader:
-            workTime = str(row[5]).split(':')
-            #print(workTime[1])
-            if int(workTime[1]) % 15 != 0:
-                print("Row #" + str(rowNum) + " is not in 15 minute increments, it reads: " + str(workTime[0]) + ":" + str(workTime[1]))
+    reader = csv.reader(fileName, delimiter='|')
+    rowNum = 1
+    for row in reader:
+        workTime = str(row[5]).split(':')
+        if int(workTime[1]) % 15 != 0:
+            print("Row #" + str(rowNum) + " is not in 15 minute increments, it reads: " + str(workTime[0]) + ":" + str(workTime[1]))
+            global errors
+            errors = 1
+    rowNum += 1
+
+def checkForOverlap(fileName):
+    reader = csv.reader(fileName, delimiter='|')
+    rowNum = 1
+    for row in reader:
+        timeIn = str(row[2]).split(':')
+        timeOut = str(row[4]).split(':')
+        hourCheck = int(timeOut[0]) - int(timeIn[0])
+        minCheck = int(timeOut[1]) - int(timeIn[1])
+        #print(hourCheck)
+        #print(minCheck)
+        if hourCheck < 0 or (hourCheck and minCheck < 0):
+            print("in row #" + str(rowNum) + " There is an inconsistency with the punch in an out times, it results in a negative.")
+            global errors
+            errors = 1
+        rowNum += 1
+
+def nameMatchCheck(fileName):
+    global fileUserName
+    reader = csv.reader(fileName, delimiter='|')
+    rowNum = 1
+    for row in reader:
+        if str(row[0]) != str(fileUserName):
+            print("Name field for row #" + str(rowNum) + " does not match file name, it says: " + str(row[0]))
+            global errors
+            errors = 1
+        rowNum += 1
+
+def checkForBlanks(fileName):
+    global hoursEntryFormat
+    reader = csv.reader(fileName, delimiter='|')
+    rowNum = 1
+    for row in reader:
+        for entry in range(len(row)):
+            if len(row[entry]) == 0:
+                print("empty field: " + hoursEntryFormat[entry] + " in row #" + str(rowNum))
                 global errors
                 errors = 1
         rowNum += 1
 
-
-
-def nameMatchCheck(fileName):
-    global fileUserName
-    with open(fileName, 'r') as csvFile:
-        reader = csv.reader(csvFile, delimiter='|')
-        rowNum = 1
-        for row in reader:
-            if str(row[0]) != str(fileUserName):
-                print("Name field for row #" + str(rowNum) + " does not match file name, it says: " + str(row[0]))
-                global errors
-                errors = 1
-            rowNum += 1
-    csvFile.close()
-
-def checkForBlanks(fileName):
-    global hoursEntryFormat
-    with open(fileName, 'r') as csvFile:
-        reader = csv.reader(csvFile, delimiter='|')
-        rowNum = 1
-        for row in reader:
-            for entry in range(len(row)):
-                #print(row[entry])
-                if len(row[entry]) == 0:
-                    print("empty field: " + hoursEntryFormat[entry] + " in row #" + str(rowNum))
-                    global errors
-                    errors = 1
-            rowNum += 1
-    csvFile.close()
-
 def checkFileDate(fileName):
-    with open(fileName, 'r') as csvFile:
-        reader = csv.reader(csvFile, delimiter='|')
+        reader = csv.reader(fileName, delimiter='|')
         rowNum = 1
         for row in reader:
-            #print(row[1])
-            #print(entry)
             global fileDate
             global errors
             if str(row[1]) != str(fileDate):
@@ -76,11 +81,9 @@ def checkFileDate(fileName):
                 print("Row #" + str(rowNum) + ", the date in the \"Date Out\" field does not match file name, it says: " + str(row[3]))
                 errors = 1
             rowNum += 1
-    csvFile.close()
 
 
 fileName = str(sys.argv[1])
-    #print(fileName)
 
 errors = 0
 actualFileName = fileName.split('/')
@@ -89,7 +92,6 @@ fileYear = fileFields[0]
 fileMonth = fileFields[1]
 fileDay = fileFields[2]
 fileDate = '-'.join(fileFields[0:-1])
-#print(fileDate)
 fileUserName = fileFields[3]
 hoursEntryFormat = ['Name', 'Date In', 'Time In', "Date Out", "Time out", "Hours Worked", "Position", "Emergency",\
                         'Billable', 'Comment']
